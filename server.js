02daae77c268c7dc73f4a36e9ca3f80a4907ba2a
Prasanna V <mail2prasannavenkat@gmail.com>
2017-06-14 17:43:54 +0530

    Create server.js

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+const path = require('path');
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+app.use(forceSSL());
 
-app.listen(process.env.PORT || 8080);
